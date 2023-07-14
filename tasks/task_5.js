a0ad76f61d455d21ba2bfe68d17af5796d519fa0
Adigwe chizoba <adigwechizoba181@gmail.com>
2023-07-14 06:58:06 -0700

    lesson 5 modified

--- tasks/task_5.js
+++ tasks/task_5.js
@@ -1,6 +1,6 @@
 //create an array of multiple elements
 
-let myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
 //use filter method to create a new array 
 
