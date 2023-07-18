e30486f1e17da36a227039e0839112f610b9972f
Adigwe chizoba <adigwechizoba181@gmail.com>
2023-07-18 07:46:51 -0700

    lesson 5

--- tasks/task_5.js
+++ tasks/task_5.js
@@ -1,4 +1,4 @@
-//create an array of multiple elements
+// //create an array of multiple elements
 
 let myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17]
 
@@ -8,3 +8,56 @@ let myFilteredArray = myArray.filter(function(element){
      return element % 3 === 0;
 });
 console.log(myFilteredArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class work
+// let student = {
+//      age: 19,
+//      name: 'hey',
+//      completedTask: {
+//       task: 'functtion',
+//       task2: 'arrays' 
+// }
+// }
+// console.log(student.completedTask.task2);
+// student['task'] = 'fun'
+
+// console.log(student['completedTask'][0]);
+// student['name'] = 'clem'
+// console.log(student); 
+
+// let {completedTask} = student
+// let [task1, task2] = completedTask
+// console.log(task1);
